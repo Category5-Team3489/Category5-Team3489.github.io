8a0a4fb05a4f5b65e375d654e96a1bbf1ed0d458
Connor <61102529+Metater@users.noreply.github.com>
2021-12-04 09:30:36 -0500

    Fix project

--- Category5-Team3489.github.io/d.bat
+++ Category5-Team3489.github.io/d.bat
@@ -1,3 +1,3 @@
 CALL build.bat
 del "E:\Projects\Visual Studio\Category5-Team3489.github.io\docs" /s /f /q
-xcopy /s /f /q /y "E:\Projects\Visual Studio\metater.github.io\metater.github.io\build" "E:\Projects\Visual Studio\Category5-Team3489.github.io\docs"
+xcopy /s /f /q /y "E:\Projects\Visual Studio\Category5-Team3489.github.io\Category5-Team3489.github.io\build" "E:\Projects\Visual Studio\Category5-Team3489.github.io\docs"
