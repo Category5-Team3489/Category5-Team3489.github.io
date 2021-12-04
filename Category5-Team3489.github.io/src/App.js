81889e3be3befe8d3515c74dae82437dba4b54de
Connor <61102529+Metater@users.noreply.github.com>
2021-12-04 10:00:33 -0500

    add stuff

--- Category5-Team3489.github.io/src/App.js
+++ Category5-Team3489.github.io/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './css/App.css';
+import {BitcoinPrice} from './components/API';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org" 
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={
+            <div>
+              Hello, World! <br/>
+              Hola!
+            </div>
+          } />
+          <Route path="/bitcoin" element = {
+            <BitcoinPrice />
+          } />
+        </Routes>
+      </Router>
+      <div className='Background'/>
     </div>
   );
 }
