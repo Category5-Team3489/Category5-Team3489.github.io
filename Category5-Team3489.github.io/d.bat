8a6bdd7b6c80a468b01e84d227612cb8181f7bde
Connor <61102529+Metater@users.noreply.github.com>
2021-12-04 11:16:08 -0500

    Deploy website

--- Category5-Team3489.github.io/d.bat
+++ Category5-Team3489.github.io/d.bat
@@ -1,3 +1,11 @@
 CALL build.bat
 del "E:\Projects\Visual Studio\Category5-Team3489.github.io\docs" /s /f /q
 xcopy /s /f /q /y "E:\Projects\Visual Studio\Category5-Team3489.github.io\Category5-Team3489.github.io\build" "E:\Projects\Visual Studio\Category5-Team3489.github.io\docs"
+
+cd "E:\Projects\Visual Studio\Category5-Team3489.github.io"
+
+git add .
+git commit -m "Deploy website"
+git push
+
+pause
